13a23a01450bd280b105dcfae9e9d5ef409bbf19
Dennis <john95272009@yahoo.com.tw>
2021-10-12 11:29:41 +0800

    微調

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/123', function(){
+    return 456;
+});
 
 Route::get('/', function(){
     return view('vuejs');
